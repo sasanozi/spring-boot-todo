ab1613a6454152d0d6ce80b517a034535ce9cdf8
SSSSAAAA <107612471+sasanozi@users.noreply.github.com>
2024-10-26 11:01:12 +0900

    feat: プロジェクトの追加機能を実装 (#1)
    * add: ProjectEntity を追加
    
    * add: Project作成用のRequestを追加
    
    * add: Project作成用のController を仮実装
    
    * add: Project Repository を追加
    
    * add: ProjectCreateService を追加
    
    * add: ProjectCreateControllerを追加
    
    * add: ProjectEntityの修正

---
+++ spring/src/main/java/com/example/todo/entity/Project.java
@@ -0,0 +1,50 @@
+package com.example.todo.entity;
+
+import java.time.LocalDateTime;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "projects")
+public class Project {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Integer id;
+
+  @Column(nullable = false, length = 255)
+  private String name;
+
+  @Column(columnDefinition = "TEXT")
+  private String summary;
+
+  @Column(name = "deleted_at",nullable = false)
+  private LocalDateTime deletedAt;
+
+  @Column(name = "created_at", nullable = false)
+  private LocalDateTime createdAt;
+
+  @Column(name = "updated_at", nullable = false)
+  private LocalDateTime updatedAt;
+
+  @PrePersist
+  protected void onCreate() {
+    LocalDateTime now = LocalDateTime.now();
+    this.createdAt = now;
+    this.updatedAt = now;
+  }
+
+  @PreUpdate
+  protected void onUpdate() {
+    this.updatedAt = LocalDateTime.now();
+  }
+
+}
