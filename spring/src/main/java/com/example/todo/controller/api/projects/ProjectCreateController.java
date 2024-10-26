15c9d4691e3c37f71d31eb1c723e15985c22d7ed
chocowein <chokoweinochoko@gmail.com>
2024-10-26 10:20:15 +0900

    add: ProjectCreateControllerを追加

--- spring/src/main/java/com/example/todo/controller/api/projects/ProjectCreateController.java
+++ spring/src/main/java/com/example/todo/controller/api/projects/ProjectCreateController.java
@@ -7,16 +7,23 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.example.todo.dto.request.projects.ProjectCreateRequest;
 import com.example.todo.entity.Project;
+import com.example.todo.service.projects.ProjectCreateService;
 import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/api/projects")
 public class ProjectCreateController {
 
+  private final ProjectCreateService projectCreateService;
+
+  public ProjectCreateController(ProjectCreateService projectCreateService) {
+    this.projectCreateService = projectCreateService;
+
+  }
+
   @PostMapping
   public ResponseEntity<Project> invoke(@Valid @RequestBody ProjectCreateRequest request) {
-    // TODO:Projectを作成するロジックを実装予定
-    Project project = new Project();
+    Project project = this.projectCreateService.invoke(request);
 
     return ResponseEntity.ok(project);
   }
