7416b898a8ad404985904a65f5c9cbea286d3a54
sasaNozi <107612471+sasanozi@users.noreply.github.com>
2024-12-07 11:16:01 +0900

    プロジェクト追加のテストコードの実装 (#3)
    * test: ProjectCreateRequest のテストを作成
    
    * test: ProjectCreateControllerのテストの作成
    
    * fix: ControllerとServiceの修正
    
    * test:ProjectCreateService のテストの作成

---
+++ spring/src/test/java/com/example/todo/controller/api/projects/ProjectCreateControllerTest.java
@@ -0,0 +1,57 @@
+package com.example.todo.controller.api.projects;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import com.example.todo.dto.request.projects.ProjectCreateRequest;
+import com.example.todo.entity.Project;
+import com.example.todo.service.projects.ProjectCreateService;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+
+@WebMvcTest(ProjectCreateController.class)
+class ProjectCreateControllerTest {
+
+  @Autowired
+  private MockMvc mockMvc;
+
+  @MockBean
+  private ProjectCreateService projectCreateService;
+
+  @Test
+  void 作成されたプロジェクトが返されること() throws Exception {
+
+    Project mockProject = new Project();
+    mockProject.setId(1);
+    mockProject.setName("name");
+    mockProject.setSummary("this is a new project");
+
+    Mockito.when(this.projectCreateService.invoke(Mockito.any(ProjectCreateRequest.class)))
+        .thenReturn(mockProject);
+
+    // リクエストボディを作成
+    String expectRequestBody = """
+        {
+            "name": "New Project",
+            "summary": "This is a new project"
+        }
+        """;
+
+    this.mockMvc
+        .perform(post("/api/projects").contentType(MediaType.APPLICATION_JSON)
+            .content(expectRequestBody))
+        .andExpect(status().isOk()).andExpect(content().contentType(MediaType.APPLICATION_JSON))
+        .andExpect(jsonPath("$.id").value(1)).andExpect(jsonPath("$.name").value("name"))
+        .andExpect(jsonPath("$.summary").value("this is a new project"));
+
+
+  }
+
+}
