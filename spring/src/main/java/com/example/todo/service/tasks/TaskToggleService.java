47c5ab59096e44d4da10981c570b24ba6e0ccd64
chocowein <chokoweinochoko@gmail.com>
2025-01-18 11:12:52 +0900

    update: Serviceの本実装

--- spring/src/main/java/com/example/todo/service/tasks/TaskToggleService.java
+++ spring/src/main/java/com/example/todo/service/tasks/TaskToggleService.java
@@ -1,13 +1,32 @@
 package com.example.todo.service.tasks;
 
+import java.time.LocalDateTime;
 import org.springframework.stereotype.Service;
 import com.example.todo.dto.response.tasks.TaskBaseResponse;
+import com.example.todo.entity.Task;
+import com.example.todo.repository.TaskRepository;
+import jakarta.persistence.EntityNotFoundException;
 
 @Service
 public class TaskToggleService {
 
+  private final TaskRepository taskRepository;
+
+  public TaskToggleService(TaskRepository taskRepository) {
+    this.taskRepository = taskRepository;
+  }
+
   public TaskBaseResponse invoke(Integer id) {
-    return null;
+
+    Task task = this.taskRepository.findById(id).orElseThrow(
+      () -> new EntityNotFoundException("Task not found with ID: " + id));
+
+    boolean isCompleted = task.getCompletedAt() != null;
+    task.setCompletedAt(isCompleted ? null : LocalDateTime.now());
+
+    Task toggleTask = this.taskRepository.save(task);
+
+    return new TaskBaseResponse(toggleTask);
   }
 
 }
