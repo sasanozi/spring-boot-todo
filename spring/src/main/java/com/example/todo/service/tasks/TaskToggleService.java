1827707c03df506baaf1852086952c71a8d29ac8
chocowein <chokoweinochoko@gmail.com>
2025-01-18 10:55:34 +0900

    add: Controllerの実装 　 - Serviceも定義のみ実装

---
+++ spring/src/main/java/com/example/todo/service/tasks/TaskToggleService.java
@@ -0,0 +1,13 @@
+package com.example.todo.service.tasks;
+
+import org.springframework.stereotype.Service;
+import com.example.todo.dto.response.tasks.TaskBaseResponse;
+
+@Service
+public class TaskToggleService {
+
+  public TaskBaseResponse invoke(Integer id) {
+    return null;
+  }
+
+}
