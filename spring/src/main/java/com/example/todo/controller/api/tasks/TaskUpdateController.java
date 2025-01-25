7c01e48ef586e45b309ebbd62ad2d09f5e5b5ce2
chocowein <chokoweinochoko@gmail.com>
2025-01-25 11:26:26 +0900

    add: Controller の実装 fix: [React] UpdateTaskSchema の修正

--- spring/src/main/java/com/example/todo/controller/api/tasks/TaskUpdateController.java
+++ spring/src/main/java/com/example/todo/controller/api/tasks/TaskUpdateController.java
@@ -1,10 +1,14 @@
 package com.example.todo.controller.api.tasks;
 
-import com.example.todo.dto.response.tasks.TaskBaseResponse;
-import com.example.todo.service.tasks.TaskUpdateService;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import com.example.todo.dto.request.tasks.TaskUpdateRequest;
+import com.example.todo.dto.response.tasks.TaskBaseResponse;
+import com.example.todo.service.tasks.TaskUpdateService;
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/api/tasks")
@@ -16,7 +20,9 @@ public class TaskUpdateController {
     this.taskUpdateService = taskUpdateService;
   }
 
-  public ResponseEntity<TaskBaseResponse> invoke(){
-
+  @PutMapping
+  public ResponseEntity<TaskBaseResponse> invoke(@Valid @RequestBody TaskUpdateRequest request) {
+    TaskBaseResponse response = this.taskUpdateService.invoke(request);
+    return ResponseEntity.ok(response);
   }
 }
--- spring/src/main/java/com/example/todo/service/tasks/TaskUpdateService.java
+++ spring/src/main/java/com/example/todo/service/tasks/TaskUpdateService.java
@@ -1,6 +1,45 @@
 package com.example.todo.service.tasks;
 
+import org.springframework.beans.BeanUtils;
+import org.springframework.stereotype.Service;
+import com.example.todo.dto.request.tasks.TaskUpdateRequest;
+import com.example.todo.dto.response.tasks.TaskBaseResponse;
+import com.example.todo.entity.Project;
+import com.example.todo.entity.Task;
+import com.example.todo.repository.ProjectRepository;
+import com.example.todo.repository.TaskRepository;
+import jakarta.persistence.EntityNotFoundException;
 
+@Service
 public class TaskUpdateService {
 
+  private final TaskRepository taskRepository;
+  private final ProjectRepository projectRepository;
+
+  public TaskUpdateService(TaskRepository taskRepository, ProjectRepository projectRepository) {
+    this.taskRepository = taskRepository;
+    this.projectRepository = projectRepository;
+  }
+
+  public TaskBaseResponse invoke(TaskUpdateRequest request) {
+
+    Task task = this.taskRepository.findById(request.getId()).orElseThrow(
+       () -> new EntityNotFoundException("Task not found with id: " + request.getId())
+    );
+
+    if (request.getProjectId() != null) {
+      Project project = this.projectRepository.findById(request.getProjectId()).orElseThrow(
+        () -> new EntityNotFoundException("Project not found with id: " + request.getProjectId())
+      );
+      task.setProject(project);
+    } else {
+      task.setProject(null);
+    }
+
+    BeanUtils.copyProperties(request, task,"id");
+
+    Task updatedTask = this.taskRepository.save(task);
+
+    return new TaskBaseResponse(updatedTask);
+  }
 }
